3d94b32de6be98b6eae6e0337fdf2ca01d8744e4
littlebabyshit <38035679+littlebabyshit@users.noreply.github.com>
2023-03-31 15:32:46 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -10,7 +10,7 @@
 enabled_site_setting :oauth2_enabled
 
 class ::OmniAuth::Strategies::Oauth2Basic < ::OmniAuth::Strategies::OAuth2
-  option :name, "oauth2_basic"
+  option :name, "oauth2_basic_phone"
 
   uid do
     if path = SiteSetting.oauth2_callback_user_id_path.split(".")
